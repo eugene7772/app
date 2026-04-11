9dbb62835364e4b6405c016f9aece7c918b7e2d9
efominykh <efominykh@neoflex.ru>
2026-04-11 22:37:09 +0300

    scale workers

--- dialog/docker/citus/bootstrap/01-bootstrap.sh
+++ dialog/docker/citus/bootstrap/01-bootstrap.sh
@@ -1,71 +1,61 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 export PGPASSWORD="$POSTGRES_PASSWORD"
+EXPECTED_WORKERS="${WORKER_COUNT:-5}"
 
 echo "Waiting for coordinator..."
 until pg_isready -h citus-master -p 5432 -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
   sleep 2
 done
 
-echo "Waiting for worker 1..."
-until pg_isready -h citus-worker-1 -p 5432 -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
+echo "Waiting for $EXPECTED_WORKERS worker containers to be running..."
+while true; do
+  COUNT=$(docker ps \
+    --filter "label=com.docker.compose.project=${COMPOSE_PROJECT_NAME}" \
+    --filter "label=com.docker.compose.service=worker" \
+    --format '{{.Names}}' | wc -l)
+
+  echo "Currently running workers: $COUNT / $EXPECTED_WORKERS"
+
+  if [ "$COUNT" -ge "$EXPECTED_WORKERS" ]; then
+    break
+  fi
+
   sleep 2
 done
 
-echo "Waiting for worker 2..."
-until pg_isready -h citus-worker-2 -p 5432 -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
-  sleep 2
-done
-
-echo "Ensuring Citus extension exists..."
-psql -h citus-master -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "CREATE EXTENSION IF NOT EXISTS citus;"
+echo "Waiting for every worker to accept connections..."
+docker ps \
+  --filter "label=com.docker.compose.project=${COMPOSE_PROJECT_NAME}" \
+  --filter "label=com.docker.compose.service=worker" \
+  --format '{{.Names}}' | while read NAME; do
+    until pg_isready -h "$NAME" -p 5432 -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
+      sleep 2
+    done
+  done
 
 echo "Setting coordinator host..."
-psql -h citus-master -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "SELECT citus_set_coordinator_host('citus-master');"
+psql -h citus-master -U "$POSTGRES_USER" -d "$POSTGRES_DB" \
+  -c "CREATE EXTENSION IF NOT EXISTS citus;"
+psql -h citus-master -U "$POSTGRES_USER" -d "$POSTGRES_DB" \
+  -c "SELECT citus_set_coordinator_host('citus-master');"
 
-echo "Adding workers if missing..."
-psql -h citus-master -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<'SQL'
-DO $$
+echo "Registering workers..."
+docker ps \
+  --filter "label=com.docker.compose.project=${COMPOSE_PROJECT_NAME}" \
+  --filter "label=com.docker.compose.service=worker" \
+  --format '{{.Names}}' | while read NAME; do
+    psql -h citus-master -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<SQL
+DO \$\$
 BEGIN
   IF NOT EXISTS (
     SELECT 1 FROM pg_dist_node
-    WHERE nodename = 'citus-worker-1' AND nodeport = 5432
+    WHERE nodename = '$NAME' AND nodeport = 5432
   ) THEN
-    PERFORM citus_add_node('citus-worker-1', 5432);
-  END IF;
-
-  IF NOT EXISTS (
-    SELECT 1 FROM pg_dist_node
-    WHERE nodename = 'citus-worker-2' AND nodeport = 5432
-  ) THEN
-    PERFORM citus_add_node('citus-worker-2', 5432);
+    PERFORM citus_add_node('$NAME', 5432);
   END IF;
 END
-$$;
+\$\$;
 SQL
-
-echo "Distributing tables if needed..."
-psql -h citus-master -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<'SQL'
-DO $$
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1
-    FROM pg_dist_partition
-    WHERE logicalrelid = 'public.dialog'::regclass
-  ) THEN
-    PERFORM create_distributed_table('public.dialog', 'id');
-  END IF;
-
-  IF NOT EXISTS (
-    SELECT 1
-    FROM pg_dist_partition
-    WHERE logicalrelid = 'public.message'::regclass
-  ) THEN
-    PERFORM create_distributed_table('public.message', 'dialog_id', colocate_with => 'public.dialog');
-  END IF;
-END
-$$;
-SQL
-
-echo "Bootstrap done"
+done
